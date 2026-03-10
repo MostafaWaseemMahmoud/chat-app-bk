194da8f424b5bff222d7e323a8e66a027691fab9
MostafaWaseemMahmoud <mostafawaseem22@gmail.com>
2026-03-11 01:21:51 +0200

    Apply cors from the origin of the app

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
- app.enableCors({
-    origin: ['https://chat-app-xi-teal-10.vercel.app','http://localhost:5173'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        'https://chat-app-xi-teal-10.vercel.app',
+        'http://localhost:5173'
+      ],
+      methods: ['GET','POST','PUT','DELETE','PATCH'],
+      credentials: true
+    }
   });
-  await app.listen(process.env.PORT ?? 3000).then(()=>{
-    console.log("running")
-  });
+
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
