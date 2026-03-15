f7f0db001965fd332af97b17c5a5516b98c30471
MostafaWaseemMahmoud <mostafawaseem22@gmail.com>
2026-03-15 22:49:09 +0200

    make Some updates for vercel

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'https://chat-app-xi-teal-10.vercel.app',
-        'http://localhost:5173'
-      ],
-      methods: ['GET','POST','PUT','DELETE','PATCH'],
-      credentials: true
-    }
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'https://chat-app-xi-teal-10.vercel.app',
+      'http://localhost:5173'
+    ],
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+    credentials: true,
   });
 
   await app.listen(process.env.PORT || 3000);
