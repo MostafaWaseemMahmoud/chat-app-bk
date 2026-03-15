1955e0ff53199c8312410389ddf646daaf78028f
MostafaWaseemMahmoud <mostafawaseem22@gmail.com>
2026-03-15 22:43:12 +0200

    make Some Updateds

--- src/rooms/rooms.contoller.ts
+++ src/rooms/rooms.contoller.ts
@@ -1,42 +1,38 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Req, Res } from "@nestjs/common";
 import { RoomsService } from "./rooms.service";
 
 @Controller('rooms')
+
 export class RoomsController {
+      constructor(private readonly  roomService: RoomsService) {}
+    
+       @Get('/') 
 
-  constructor(private readonly roomService: RoomsService) {}
+       sayHi(@Res() res) {
+            res.status(200).json({message: "Rooms Root Is Success"})
+       }
 
-  @Get('/')
-  sayHi() {
-    return { message: "Rooms Root Is Success" };
-  }
+       @Post("createroom")
 
-  @Post("createroom")
-  createRoom(@Body() body) {
-    return this.roomService.create_room(body);
-  }
+       CreateRoom(@Req() req, @Res() res){
+        return this.roomService.create_room(req,res)
+       }
 
-  @Get("allrooms/:id")
-  findUsersRoom(@Param('id') id: string) {
-    return this.roomService.getUsersRooms(id);
-  }
+       @Get("allrooms/:id")
 
-  @Post("sendmessage/:roomid/:userid")
-  sendMessage(
-    @Param('roomid') roomid: string,
-    @Param('userid') userid: string,
-    @Body("message") message: string,
-    @Body("email") email: string,
-    @Body("date") date: string
-  ) {
-    return this.roomService.sendMessage(roomid, message, email, userid, date);
-  }
+       FindUsersRoom(@Param('id') id, @Res() res){
+        return this.roomService.getUsersRooms(res,id)
+       }
 
-  @Post("makeasread/:roomid/:userid")
-  makeAsRead(
-    @Param('roomid') roomid: string,
-    @Param('userid') userid: string
-  ) {
-    return this.roomService.moveNewMessagesToMessages(roomid, userid);
-  }
+       @Post("sendmessage/:rooomid/:userid")
+
+       sendMessage(@Param('rooomid') roomid,@Param('userid') userid, @Res() res,@Body("message") message,@Body("email") email,@Body("date") date){
+        return this.roomService.sendMessage(res,roomid,message,email,userid,date);
+       }
+
+       @Post("makeasread/:rooomid/:userid")
+
+       MakeAsRead(@Param('rooomid') roomid,@Param('userid') userid,@Res() res){
+        return this.roomService.moveNewMessagesToMessages(res,roomid,userid);
+       }
 }
